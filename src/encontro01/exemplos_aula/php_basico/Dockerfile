4c53fb38dbe9dfa4beb1e5c22ce859810d6c26cb
Murilo-ZC <carvalho.zanini@gmail.com>
2023-08-02 00:53:27 -0300

    Adição do primeiro exemplo de aula

---
+++ src/encontro01/exemplos_aula/php_basico/Dockerfile
@@ -0,0 +1,5 @@
+FROM php:7.4-apache
+
+COPY index.php /var/www/html/
+
+EXPOSE 80
---
+++ src/encontro01/exemplos_aula/php_basico/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Olá, Mundo!</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="jumbotron">
+            <h1 class="display-4">Olá, Mundo!</h1>
+            <p class="lead">Esta é uma página básica de exemplo estilizada com Bootstrap.</p>
+            <hr class="my-4">
+            <p>Você pode editar este arquivo index.php para criar suas próprias páginas.</p>
+            <a class="btn btn-primary btn-lg" href="https://getbootstrap.com/docs/4.5/getting-started/introduction/" target="_blank" role="button">Saiba mais sobre o Bootstrap</a>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
